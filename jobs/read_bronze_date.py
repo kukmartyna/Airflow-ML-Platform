0fa876310b693cf0e579f2cdf09d7c3fa2ac7d7a
martynakuk <s25823@pjwstk.edu.pl>
2026-01-11 23:58:46 +0100

    Added bronze to silver transformation for single table.

--- jobs/read_bronze_date.py
+++ jobs/read_bronze_date.py
@@ -1,32 +1,61 @@
-import sys
-import traceback
-
-from py4j.protocol import Py4JJavaError
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, Window, functions as F
 
 
 def main():
     MINIO_ENDPOINT = 'http://host.docker.internal:9000'
     MINIO_ACCESS_KEY = 'admin'
     MINIO_SECRET_KEY = 'password'
+    MINIO_BUCKET = 'default'
 
-    spark = (SparkSession.builder
-             .appName('PySpark example')
-             .config("spark.hadoop.fs.s3a.endpoint", MINIO_ENDPOINT)
-             .config("spark.hadoop.fs.s3a.access.key", MINIO_ACCESS_KEY)
-             .config("spark.hadoop.fs.s3a.secret.key", MINIO_SECRET_KEY)
-             .config("spark.hadoop.fs.s3a.path.style.access", "true")
-             .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-             .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
-             .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-             .getOrCreate())
+    with (SparkSession.builder
+            .appName('PySpark example')
+            .config("spark.hadoop.fs.s3a.endpoint", MINIO_ENDPOINT)
+            .config("spark.hadoop.fs.s3a.access.key", MINIO_ACCESS_KEY)
+            .config("spark.hadoop.fs.s3a.secret.key", MINIO_SECRET_KEY)
+            .config("spark.hadoop.fs.s3a.path.style.access", "true")
+            .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+            .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+            .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+            .getOrCreate()) as spark:
+        CATALOG = 'bronze'
+        SCHEMA = 'public'
 
-    delta_path = "s3a://bronze/MDP.public.klienci"
+        bronze_path = f"s3a://{MINIO_BUCKET}/bronze/public/klienci"
+        silver_path = f"s3a://{MINIO_BUCKET}/silver/public/klienci"
 
-    df = spark.read.format("delta").load(delta_path)
-    df.show()
+        df = spark.read.format("delta").load(bronze_path)
+        df.show()
 
-    spark.stop()
+        df = df.withColumn("payload", F.col("after"))
+
+        df = df.select(
+            F.col("payload"),
+            F.col("source.lsn"),
+            F.col("op"),
+        )
+
+        pk = ['payload.id_klienta']
+        w = Window.partitionBy(*pk).orderBy(F.col("lsn").desc_nulls_last())
+
+        df = df.withColumn("rn", F.row_number().over(w))
+        df.show()
+        df = df.filter(
+            (F.col("rn") == 1)
+            & (F.col("op") != 'd')
+        )
+        df = df.select(F.col("payload.*"))
+
+        df.show()
+
+        # before = df.select(F.col('before.*'))
+        # after = df.select(F.col('after.*'))
+        # source = df.select(F.col('source.*'))
+
+        # before.show()
+        # after.show()
+        # source.show()
+
+        df.write.format("json").mode("overwrite").save(silver_path)
 
 
 if __name__ == "__main__":
